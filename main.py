daecc87502693e7343d8df3eeb07fa9c87e4fbcc
velinamons <velinamons@gmail.com>
2025-03-20 20:19:47 +0200

    Remove redundant comment on fetch_klines_batch()

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from logger.config import logger
 
 async def main():
     logger.info("Starting app")
-    # await fetch_klines_batch()
+    # TODO: implement check of already downloaded parquet klines
+    await fetch_klines_batch()
     logger.info("Starting bot")
     await start_bot()
 
